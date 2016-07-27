f217b0f3163c9ec10a0aeb54b91d0e20ebbc9e01
taolichao <xvhandong@sina.com>
2016-07-27 15:13:21 +0800

    文件名和链接地址修改

--- WebContent/guliPC/js/base.js
+++ WebContent/guliPC/js/base.js
@@ -1,5 +1,18 @@
 // JavaScript Document
+//把null或undefined转换成字符串
+function typeNullFoString(str){
+	if("undefined" == typeof(str) || !str){
+		return '';
+	}
+	return str;
+}
 
+//获取url中的参数
+function getQueryString(name){
+var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+if (r!=null) return unescape(r[2]); return null; //返回参数值
+} 
 //搜索框
 $(document).ready(function() {
 	$("#skeyword").val('搜索');
